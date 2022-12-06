e696349d2a50d057cd489d9bff31ad8ac2d60191
kotadakku <huunv1704@gmail.com>
2022-12-06 14:06:58 +0700

    Initial Commit

---
+++ iHelia2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iHelia2
+//
+//  Created by Nguyen Huu on 06/12/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iHelia2/iHelia2App.swift
@@ -0,0 +1,17 @@
+//
+//  iHelia2App.swift
+//  iHelia2
+//
+//  Created by Nguyen Huu on 06/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iHelia2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
